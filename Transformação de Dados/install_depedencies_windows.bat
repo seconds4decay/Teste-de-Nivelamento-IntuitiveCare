17d455181e7b861a3f95b042f551686efa35254b
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2025-03-31 15:58:02 -0300

    Scripts de Instalação de Depedencias

--- Transformação de Dados/install_depedencies_windows.bat
+++ Transformação de Dados/install_depedencies_windows.bat
@@ -1,5 +1,7 @@
 @echo off
 python -m pip install --upgrade pip
 python -m pip install selenium
+python -m pip install pdfplumber
+python -m pip install pandas
 echo Instalação concluída!
 pause
--- Transformação de Dados/install_dependencies_linux.sh
+++ Transformação de Dados/install_dependencies_linux.sh
@@ -2,4 +2,8 @@ python3 -m pip install --upgrade pip
 
 python3 -m pip install selenium
 
+python3 -m pip install pdfplumber 
+
+python3 -m pip install pandas
+
 echo "Instalação concluída!"
