4126f8cc8641416b6e0c7007fec012f445a52107
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2025-04-03 21:21:16 -0300

    Ajustes na API

--- API/backend/api.py
+++ API/backend/api.py
@@ -33,8 +33,17 @@ def buscar_operadora(dados: BuscaOperadora):
     filtro = pd.Series([False] * len(df))
 
     if dados.registro is not None:
+        if(not dados.registro.isnumeric()):
+            return {"error": "O registro deve conter apenas números."}
+
         filtro |= df["Registro_ANS"].astype(str).str.contains(str(dados.registro), na=False, case=False)
     if dados.cnpj is not None:
+        if(not dados.cnpj.isnumeric()):
+            return {"error": "O CNPJ deve conter apenas números."}
+        
+        if(len(dados.cnpj) != 14):
+            return {"error": "O CNPJ deve conter 14 dígitos."}
+
         filtro |= df["CNPJ"].astype(str).str.contains(str(dados.cnpj), na=False, case=False)
     if dados.razao_social is not None:
         filtro |= df["Razao_Social"].astype(str).str.contains(str(dados.razao_social), na=False, case=False)
